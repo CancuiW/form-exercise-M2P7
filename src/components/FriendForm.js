1cea6ceefe68c176f18f51dedd11480c902c086b
CancuiW <can.cui@wayne.edu>
2023-07-11 23:29:47 -0700

    exercise 1 in M2S7

--- src/components/FriendForm.js
+++ src/components/FriendForm.js
@@ -9,36 +9,51 @@ export default function FriendForm(props) {
     // a) pull the name of the input from the event object
     // b) pull the value of the input from the event object
     // c) use the `update` callback coming in through props
+    const {name,value}=evt.target
+    update(name,value)
+    
   }
 
   const onSubmit = evt => {
     // 🔥 STEP 7 - IMPLEMENT the submit handler
     // a) don't allow the browser to reload!
     // c) use the `submit` callback coming in through props
+    evt.preventDefault()
+    submit()
   }
 
   return (
     <form className='form container' onSubmit={onSubmit}>
       <div className='form-group inputs'>
-        {/* ////////// TEXT INPUTS ////////// */}
-        {/* ////////// TEXT INPUTS ////////// */}
-        {/* ////////// TEXT INPUTS ////////// */}
+       
         <label>Username
           {/* 🔥 STEP 3 - Make an input of type `text` for username.
               Controlled inputs need `value` and `onChange` props.
               Inputs render what they're told - their current value comes from app state.
-              At each keystroke, a change handler fires to change app state. */}
+              At each keystroke, a change handler fires to change app state. */
+          }
+          <input type='text' value={values.username} onChange={onChange} 
+          placeholder='type a user name ...'  name='username' maxLength='20'
+           />
         </label>
 
         <label>Email
           {/* 🔥 STEP 4 - Make an input of type `email` or `text` for email. */}
+          <input type='email' name='email' placeholder='type an email ...'
+           value={values.email}  onChange={onChange}/>
         </label>
 
-        {/* ////////// DROPDOWN ////////// */}
-        {/* ////////// DROPDOWN ////////// */}
-        {/* ////////// DROPDOWN ////////// */}
+       
         <label>Role
           {/* 🔥 STEP 5 - Make dropdown for role. */}
+          <select  name='role' onChange={onChange} value={values.role}>
+            <option value="">---Select a Role---</option>
+            <option value="student">Student</option>
+            <option value="instructor">Instructor</option>
+            <option value="alumni">Alumni</option>
+            
+
+          </select>
         </label>
 
         <div className='submit'>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React ,{useState}from 'react'
 import { render } from 'react-dom'
 // 👉 App contains a more sophisticated form we'll flesh out later
 import App from './components/App'
@@ -11,13 +11,53 @@ const petsList = [
 ]
 
 function SimpleForm() {
-  return <div>Ready to start GP!</div>
+  const [pet,setPet]=useState(petsList)
+  const [formValue, setFormValue] = useState({ petName: '', petType: '' })
+  
+  const change=evt=>{
+    const {name,value}=evt.target
+    setFormValue({...formValue,[name]:value})
+
+  }
+  //Array1=[1,2,3] Array2=[7,8,9]
+  //Array3=Array1.concat(Array2)
+  //Array3=[1,2,3,7,8,9]
+  //Array is a method to conbine two array 
+  const onSubmit=evt=>{
+    evt.preventDefault()
+    setPet(pet.concat({petName:formValue.petName,petType:formValue.petType}))
+    //initialize the input content
+    setFormValue({ petName: '', petType: '' })
+
+  }
+  return (
+    <div className='container'>
+      <h1>Simple Form App</h1>
+
+      {
+        pet.map((x,idx) => {
+          return (
+            <p key={idx}>{x.petName} is a  {x.petType}</p>
+          )
+          
+        })
+      }
+      <form onSubmit={onSubmit}>
+        <input type='text' name='petName' value={formValue.petName} onChange={change} />
+        <input type='text' name='petType' value={formValue.petType} onChange={change} />
+        <input type='submit' value='create a pet'/>
+      </form>
+
+
+    </div>
+    
+  )
 }
 
 render(
   <>
     <SimpleForm />
-    {/* <App /> */}
+    <App /> 
   </>
   , document.querySelector('#root')
 )
